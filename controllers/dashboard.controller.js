8db6ec19122d24214f4d1fb73361e630822ee33f
UinTus <tuuyen294@gmail.com>
2025-08-01 18:47:29 +0700

    feat: add export top products

--- controllers/dashboard.controller.js
+++ controllers/dashboard.controller.js
@@ -5,6 +5,7 @@ const { sortParser } = require("../utils/commonUtils");
 const { TOP_PRODUCTS_LIMIT } = require("../constants/dashboard");
 const { generateChartLabels } = require("../utils/dashboardUtils");
 
+
 /**
  * @function getAll
  * @description Get all the data for the dashboard
@@ -128,7 +129,7 @@ exports.exportTopProductsExcel = async (ctx) => {
     }
 
     // Generate the Excel buffer
-    const buffer = await dashboardModel.generateTopProductsExcelBuffer(
+    const buffer = await dashboardModel.getTopProductsExcelBuffer(
       products
     );
 
